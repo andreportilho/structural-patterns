e33a2736bf9ab4bbce42533a3fd7eb6afb7e78f8
Andre Portilho <andre.portilhoo@gmail.com>
2022-04-20 16:49:02 -0300

    Adapter using HDMI/VGA example

--- src/main/java/br/com/cod3r/adapter/tvPort/Client.java
+++ src/main/java/br/com/cod3r/adapter/tvPort/Client.java
@@ -1,6 +1,9 @@
 package br.com.cod3r.adapter.tvPort;
 
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToOldMonitorAdapter;
+import br.com.cod3r.adapter.tvPort.adapters.HDMIToVGAAdapter;
 import br.com.cod3r.adapter.tvPort.devices.Computer;
+import br.com.cod3r.adapter.tvPort.devices.OldMonitor;
 import br.com.cod3r.adapter.tvPort.devices.TV;
 
 public class Client {
@@ -12,11 +15,17 @@ public class Client {
 		pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 		
 		System.out.println("------ Monitor ----------");
-		
+		Computer pc2 = new Computer();
+		OldMonitor monitor = new OldMonitor();
+		pc2.connectPort(new HDMIToVGAAdapter(monitor));
+		pc2.sendImageAndSound("Cat and Rainbow", "Nyan cat song");
 
 
 		System.out.println("------ Monitor Class Adapter ----------");
-		
+		Computer pc3 = new Computer();
+		HDMIToOldMonitorAdapter monitorAdapter = new HDMIToOldMonitorAdapter();
+		pc3.connectPort(monitorAdapter);
+		pc3.sendImageAndSound("Cat and rainbow", "Nyan cat song");
 
 	}
 }
